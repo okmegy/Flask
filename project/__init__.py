ea09fe4a836e94a8b28f8412962261445d16dad1
jasper <okmegy@gotmail.com>
2017-07-06 19:16:31 +0800

    [Flask] 1.Index

--- project/__init__.py
+++ project/__init__.py
@@ -1,17 +1,23 @@
 # project/__init__.py
 
 from flask import Flask, jsonify
+from flask import render_template
 
 # instantiate the app
 app = Flask(__name__)
 
-#set config
+# set config
 app.config.from_object('project.config.DevelopmentConfig')
 
 
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+@app.route('/', methods=['GET'])
+def index():
+    return render_template("index.html")
+
+@app.route('/hello/<name>')
+def hello(name):
+    return "Hello %s" % name
+
+# entry
+if __name__ == '__main__':
+    app.run(debug=True)
