eff7005da367d9fc7fb1feb085c97823bded11d1
jasper <okmegy@gotmail.com>
2017-07-06 13:55:06 +0800

    [Flask] 1. Add configuration

--- project/__init__.py
+++ project/__init__.py
@@ -1,13 +1,14 @@
 # project/__init__.py
-
 from flask import Flask, jsonify
 
 # instantiate the app
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 
-#set config
-app.config.from_object('project.config.DevelopmentConfig')
+# Load the default configuration
+app.config.from_object('config')
 
+# Load the configuration from the instance folder
+app.config.from_pyfile('config.py')
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
@@ -15,3 +16,5 @@ def ping_pong():
         'status': 'success',
         'message': 'pong!'
     })
+
+app.config.from_envvar('APP_CONFIG_FILE')
---
+++ start.sh
@@ -0,0 +1,4 @@
+# start.sh
+
+export APP_CONFIG_FILE=/home/jasper/flask-microservices-users/project/config/production.py
+python run.py
